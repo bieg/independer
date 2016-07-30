15be9f2fb684cc788cd932f6ef7da3622bd7cc2d
bieg <BIEG@IMAGESTYLER.local>
2016-07-30 20:47:56 +0200

    x
    x

--- script.js
+++ script.js
@@ -36,6 +36,15 @@ module.exports = new Script({
                 }
             },
 
+            finish: {
+        receive: (bot, message) => {
+            return bot.getProp('name')
+                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+                        'teach me how to do anything else!'))
+                .then(() => 'finish');
+        }
+    }
+
     speak: {
         receive: (bot, message) => {
 
