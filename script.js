6a035713e0f0cb76917a93f073697dbdf4ec7831
bieg <mailme@bieg.nl>
2016-07-31 18:17:48 +0200

    k
    k

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ module.exports = new Script({
                     const name = message.text;
                     return bot.setProp('name', name)
                         .then(() => bot.say(`${groet} ${name}, hoe kan ik je helpen? \n
-                         %[Heb je schade](postback:schade) %[Zoek je een verzekering](postback:zoek)
+                        .then(() => bot.say(`    %[Heb je schade](postback:schade) %[Zoek je een verzekering](postback:zoek)
                           `))
                         .then(() => 'done');
                 }
@@ -68,6 +68,8 @@ module.exports = new Script({
     schade: {
           prompt: (bot) => bot.say('Pleasure meeting you'),
           receive: (bot, message) => {
+
+
           .then (() => 'processing')
 
     },
