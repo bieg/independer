fdc6e258f3c24aa308f09d21f203598a191a9154
bieg <mailme@bieg.nl>
2016-07-31 12:39:19 +0200

    script
    no next

--- script.js
+++ script.js
@@ -55,8 +55,7 @@ module.exports = new Script({
                     return bot.setProp('name', name)
                         .then(() => bot.say(`${groet} ${name}, hoe kan ik je helpen?
                             %[Ik heb schade](postback:schade) %[No](postback:no)
-                          `))
-                        .then(() => 'next');
+                          `));
                 }
             },
 
