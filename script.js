a138d989c7c118743731e9cfd8722f1ea611e9d6
bieg <mailme@bieg.nl>
2016-07-31 18:56:29 +0200

    k
    k

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ module.exports = new Script({
                         .then(() => bot.say(`${groet} ${name}, hoe kan ik je helpen? \n
                          %[Heb je schade](postback:schade) %[Zoek je een verzekering](postback:zoek)
                           `))
-                        .then(() => 'done');
+                        .then(() => 'speak');
                 }
             },
 
