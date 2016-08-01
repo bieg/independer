39ef7bf5c6cfbea88faad840320b90a2669f552d
bieg <mailme@bieg.nl>
2016-08-01 23:41:24 +0200

    w
    w

--- script.js
+++ script.js
@@ -28,68 +28,121 @@ else  /* the hour is not between 0 and 24, so something is wrong */
 {
 groet = "Welkom";
 }
+${groet}
 
+
+function wait(ms) {
+return new Promise((resolve) => {
+setTimeout(resolve, ms);
+});
+}
 
 module.exports = new Script({
-    processing: {
-        //prompt: (bot) => bot.say('Beep boop...'),
-        receive: () => 'processing'
-    },
+processing: {
+//prompt: (bot) => bot.say('Beep boop...'),
+receive: () => 'processing'
+},
 
-    start: {
-        receive: (bot) => {
-            return bot.say('${groet} So you want to learn about Esther? Just say HELLO to get started.')
-                .then(() => 'speak');
-        }
-    },
+start: {
+receive: (bot) => {
+return bot.say(`${groet} - welkom bij Independer Wil je verder? %[Yes](postback:askName) %[No](postback:bye)`)
+.then(() => 'processing');
+}
+},
 
-    speak: {
-        receive: (bot, message) => {
+bye: {
+prompt: (bot) => bot.say('Postback is working'),
+receive: () => 'processing'
+},
 
-            let upperText = message.text.trim().toUpperCase();
+/*
+askName: {
+prompt: (bot) => bot.say('Hoe heet je?'),
+receive: (bot,message) => {
+const name = message.text;
+return bot.setProp('name', name)
+.then(() => bot.say(`Ok ${name}, hoe kan ik je helpen? \n
+%[Heb je schade](postback:schade) %[Zoek je een verzekering](postback:zoek)`))
+.then(() => 'processing');
+},
+},
+*/
 
-            function updateSilent() {
-                switch (upperText) {
-                    case "CONNECT ME":
-                        return bot.setProp("silent", true);
-                    case "DISCONNECT":
-                        return bot.setProp("silent", false);
-                    default:
-                        return Promise.resolve();
-                }
-            }
 
-            function getSilent() {
-                return bot.getProp("silent");
-            }
-
-            function processMessage(isSilent) {
-                if (isSilent) {
-                    return Promise.resolve("speak");
-                }
-
-                if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
-                }
-
-                var response = scriptRules[upperText];
-                var lines = response.split('\n');
-
-                var p = Promise.resolve();
-                _.each(lines, function(line) {
-                    line = line.trim();
-                    p = p.then(function() {
-                        console.log(line);
-                        return bot.say(line);
-                    });
-                })
-
-                return p.then(() => 'speak');
-            }
-
-            return updateSilent()
-                .then(getSilent)
-                .then(processMessage);
-        }
+askName: {
+    prompt: (bot) => bot.say('Postback is working. What\'s your name?'),
+    receive: (bot, message) => {
+        const name = message.text;
+        return bot.setProp('name', name)
+            .then(() => bot.say(`Great! I'll call you ${name}
+Is that OK? %[Yes](postback:bye) %[No](postback:bye)`))
+            .then(() => 'finish');
     }
+},
+
+zoek: {
+prompt: (bot) => bot.say('Postback is working'),
+receive: () => 'processing'
+},
+
+error: {
+prompt: (bot) => bot.say('Sorry - kun je dat nog eens zeggen?  Er ging iets mis...'),
+receive: () => ''
+},
+
+finish: {
+receive: () => 'finish'
+},
+
+speak: {
+receive: (bot, message) => {
+
+let upperText = message.text.trim().toUpperCase();
+
+function updateSilent() {
+switch (upperText) {
+case "CONNECT ME":
+return bot.setProp("silent", true);
+case "DISCONNECT":
+return bot.setProp("silent", false);
+default:
+return Promise.resolve();
+}
+}
+
+function getSilent() {
+return bot.getProp("silent");
+}
+
+function processMessage(isSilent) {
+if (isSilent) {
+return Promise.resolve("speak");
+}
+
+if (!_.has(scriptRules, upperText)) {
+return bot.say(`Wanna move on %[yes](ja) %[no](no)?`).then(() => 'speak');
+}
+
+var response = scriptRules[upperText];
+var lines = response.split('\n');
+
+var p = Promise.resolve();
+_.each(lines, function(line) {
+line = line.trim();
+p = p.then(function() {
+console.log(line);
+return wait(50).then(function() {
+return bot.say(line);
+});
+});
+});
+
+return p.then(() => 'speak');
+}
+
+return updateSilent()
+.then(getSilent)
+.then(processMessage);
+}
+}
 });
