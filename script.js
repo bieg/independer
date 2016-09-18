c1e2dc535e40b04b2ee882b36e927a24cf8a1bd2
bieg <mailme@bieg.nl>
2016-09-18 12:32:57 +0200

    d
    d

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ receive: (bot,message) => {
 //  if (name.indexOf("schade") >= 0){console.log('woord zit erin')}
 
       .then(() => bot.say(`Great! I\'ll call you ${name}`))
-.then(() =>  bot.say(`${groet} - welkom bij Independer.%[Star Trek](reply:STAR_TREK)`))
+.then(() =>  bot.say(`${groet} - welkom bij Independer.  %[Button label here](http://anyurl.com)`))
 .then(() => 'askName');
 }
 },
