da4b87f2541bc616e0da0e4b6e4c58d505a597bd
bieg <mailme@bieg.nl>
2016-09-20 23:42:23 +0200

    d
    d

--- script.js
+++ script.js
@@ -43,13 +43,11 @@ prompt: (bot) => bot.say('...'),
  },
 
 start: {
-    receive: (bot) => {}
-        prompt: (bot) => bot.say('ik ben de IndependerBot. Met wie heb ik het genoegen?');
-
+    prompt: (bot) => bot.say('ik ben de IndependerBot. Met wie heb ik het genoegen?'),
     receive: (bot,message) => {
         const naam = message.text;
         return bot.setProp('naam', naam)
-            .then(() => bot.say(`Wat voor soort hypotheek zoek je ${naam}? %[Starters hypotheek](postback:askName) %[Nieuwe hypotheek](postback:other)`)
+            .then(() => bot.say(`Wat voor soort hypotheek zoek je ${naam}? %[Starters hypotheek](postback:askName) %[Nieuwe hypotheek](postback:other)`))
             .then(() => 'vraagEEN');
     }
 },
