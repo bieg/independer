ba2508f65562d98c428a5754b0e516cf0a1ef2bc
bieg <mailme@bieg.nl>
2016-09-26 00:14:09 +0200

    script
    l

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ selecteerHypotheek: {
           return bot.say(`Daar help ik je graag bij. Weet je al wat voor soort woning?`)
           .then(() => bot.say(`![](http://www.bieg.nl/beeld/woningen.jpg)`))
             .then(() => bot.say(`%[Appartement](postback:hypotheekkeuze_appartement) %[Tussenwoning](postback:hypotheekkeuze_tussenwoning) %[Vrijstaand](postback:hypotheekkeuze_vrijstaand)`))
-            .then(() => 'askName')
+            .then(() => 'processing')
           break;
         case 'Nieuwe hypotheek':
           return bot.say(`Helaas biedt Independer momenteel alleen Starters een hypotheek aan.`)
