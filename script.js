94fd7cce38fc7a20abf23becaa9b334d1639f1d5
bieg <mailme@bieg.nl>
2016-09-26 00:16:58 +0200

    scritp
    pk

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ selecteerHypotheek: {
           return bot.say(`Daar help ik je graag bij. Weet je al wat voor soort woning?`)
           .then(() => bot.say(`![](http://www.bieg.nl/beeld/woningen.jpg)`))
             .then(() => bot.say(`%[Appartement](postback:hypotheekkeuze_appartement) %[Tussenwoning](postback:hypotheekkeuze_tussenwoning) %[Vrijstaand](postback:hypotheekkeuze_vrijstaand)`))
-            .then(() => 'processing')
+            .then(() => 'vervolgVragen')
           break;
         case 'Nieuwe hypotheek':
           return bot.say(`Helaas biedt Independer momenteel alleen Starters een hypotheek aan.`)
@@ -84,15 +84,15 @@ selecteerHypotheek: {
 },
 hypotheekkeuze_appartement: {
   //  prompt: (bot) => bot.say(`![](http:www.bieg.nl/beeld/appartement.jpg)`)
-    receive: () => 'processing'
+    receive: () => 'askName'
 },
 hypotheekkeuze_tussenwoning: {
 //    prompt: (bot) => bot.say(`![](http:www.bieg.nl/beeld/appartement.jpg)`)
-    receive: () => 'processing'
+  receive: () => 'askName'
 },
 hypotheekkeuze_vrijstaand: {
 //    prompt: (bot) => bot.say(`![](http:www.bieg.nl/beeld/appartement.jpg)`)
-    receive: () => 'processing'
+    receive: () => 'askName'
 },
 hypotheektype_nieuw: {
 //    prompt: (bot) => bot.say(`Independer biedt momenteel alleen voor Starters een hypotheek. Onderstaande link bied je meer informatie %[Hypotheek Adviseur](https://www.independer.nl/hypotheekadviseur/intro.aspx)`),
