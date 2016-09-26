6ad5f8d8d33f94eb9cd0ccc34a346008ac230eb8
bieg <mailme@bieg.nl>
2016-09-27 00:14:44 +0200

    w
    w

--- script.js
+++ script.js
@@ -144,12 +144,12 @@ askName: {
 
 lastCheck: {
     prompt: (bot) => bot.say('Is er nog iets waar ik  je bij kan helpen? %[JA](postback:nogietsanders) %[NEE](postback:bye) '),
-    .then(()  => 'processing'
+    .then(()  => 'processing');
 },
 
 bye: {
     prompt: (bot) => bot.say('Fijn je gesproken te hebben. Bedankt voor je tijd'),
-    .then(()  => 'processing'
+    receive: ()  => 'finish'
 },
 
 
