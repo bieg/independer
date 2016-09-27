80c54b9f4d9056131d19c923d2daf5fb6ef7cecd
bieg <mailme@bieg.nl>
2016-09-27 22:44:29 +0200

    2e
    e2

--- script.js
+++ script.js
@@ -231,28 +231,3 @@ receive: () => 'finish'
           }
       }
 });
-
-const userId = 'testUserId';
-const store = new MemoryStore();
-const lock = new MemoryLock();
-const bot = new ConsoleBot({
-    store,
-    lock,
-    userId
-});
-
-const stateMachine = new StateMachine({
-    script,
-    bot,
-    userId
-});
-
-process.stdin.on('data', function(data) {
-    stateMachine.receiveMessage({
-        text: data.toString().trim()
-    })
-        .catch((err) => {
-            console.error(err);
-            console.error(err.stack);
-        });
-});
