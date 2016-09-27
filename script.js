26a07d0301a9cbd19be752e1e8af8170bc8f1762
bieg <mailme@bieg.nl>
2016-09-28 00:08:21 +0200

    wd
     dw

--- script.js
+++ script.js
@@ -101,13 +101,12 @@ updateOntvangen: {
 },
 
 update_ja: {
-    // prompt: (bot) => bot.say(`Laat je dan even je email achter? Dan houden we je op de hoogte...`)
-    // receive: (bot, message) => {
-    //   const emailVisitor : message.text;
-    //   return bot.setProp('emailVisitor', emailVisitor)
-    //   .then(()  => bot.say(`Ok - dan hou ik je via ${emailVisitor} op de hoogte.`))
-    //   .then(() => 'lastCheck');
-    // }
+    receive: (bot, message) => {
+      const emailVisitor : message.text;
+      return bot.setProp('emailVisitor', emailVisitor)
+      .then(()  => bot.say(`Ok - dan hou ik je via ${emailVisitor} op de hoogte.`))
+      .then(() => 'processing');
+    }
 },
 
 
