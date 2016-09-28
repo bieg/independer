24048b79d5dbe1a02aba8ce3b157d558f057ab24
bieg <mailme@bieg.nl>
2016-09-29 00:28:52 +0200

    vwfv
    wfvw

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ selecteerHypotheek: {
 },
 
 toonOverzicht: {
-        return bot.say('![](http:www.bieg.nl/beeld/woningen.jpg)'))
+        return bot.say('![](https://github.com/bieg/independerChat/blob/master/img/Stride.png)'))
 },
 
 updateOntvangen: {
