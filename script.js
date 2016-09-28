3f4b13fe3389af7eaa3165c321d43c2bbc0b00fb
bieg <mailme@bieg.nl>
2016-09-28 21:49:22 +0200

    wd
    dw

--- script.js
+++ script.js
@@ -103,8 +103,9 @@ updateOntvangen: {
 update_ja: {
   prompt: (bot) => bot.say('Wat is je email adres?'),
       receive: (bot, message) => {
-        return bot.say('...');
-  //          const emailVisitor = message.text;
+          const emailVisitor = message.text;
+          return bot.setProp('emailVisitor', emailVisitor)
+
   //          //return bot.setProp('emailVisitor', emailVisitor)
   //           .then(()  => bot.say('Ok - dan hou ik je via ${emailVisitor} op de hoogte.'))
   //           .then(()  =>'processing')
