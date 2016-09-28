b17cf37996b84848700f6bedf2624f8c7f844e7c
bieg <mailme@bieg.nl>
2016-09-28 21:14:27 +0200

    script
    pp

--- script.js
+++ script.js
@@ -101,13 +101,13 @@ updateOntvangen: {
 },
 
 update_ja: {
-  prompt: (bot) => bot.say('Wat is email adres?')
-  //      receive: (bot, message) => {
+  prompt: (bot) => bot.say('Wat is je email adres?')
+      receive: (bot, message) => {
   //          const emailVisitor = message.text;
   //          //return bot.setProp('emailVisitor', emailVisitor)
   //           .then(()  => bot.say('Ok - dan hou ik je via ${emailVisitor} op de hoogte.'))
   //           .then(()  =>'processing')
-  //  }
+    }
 },
 
 hypotheekkeuze_appartement: {
