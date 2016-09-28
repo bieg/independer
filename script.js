6200ff10a5e74747a4cafca8d0769cd5cf13bc93
bieg <mailme@bieg.nl>
2016-09-28 22:39:05 +0200

    f1ew
    fe1

--- script.js
+++ script.js
@@ -165,7 +165,7 @@ vervolgVragen: {
 },
 
 lastCheck: {
-    prompt: (bot) => bot.say('Is er nog iets waar ik  je bij kan helpen?  %[Ja..](postback:start) %[Nee dank je](postback:bye )'),
+    prompt: (bot) => bot.say('Is er nog iets waar ik  je bij kan helpen?  %[Ja..](postback:start) %[Nee dank je](postback:finish)'),
           receive: (bot, message) => {
             switch(message.text) {
               case 'Nee hoor':
