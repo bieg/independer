126be382d01d8a439934fdf1a337bfc9b37bd256
bieg <mailme@bieg.nl>
2016-09-30 00:16:04 +0200

    feq
    ref

--- script.js
+++ script.js
@@ -50,9 +50,9 @@ prompt: (bot) =>  bot.say('![](http://www.bieg.nl/beeld/speechbubble.gif)')),
 start: {
     receive: (bot,message) => {
         const opening = message.text.trim().toUpperCase();
-        return bot.say('${groet}... Wat voor soort hypotheek zoek je?')
-        .then(() => bot.say('![](http://www.bieg.nl/beeld/woningen.jpg)'))
-        .then(() => bot.say('%[Starters Hypotheek](postback:hypotheektype_starter) %[Nieuwe hypotheek](postback:hypotheektype_nieuw) %[Hypotheek oversluiten](postback:hypotheektype_oversluiten)'))
+        return bot.say(`${groet}... Wat voor soort hypotheek zoek je? `)
+        .then(() => bot.say(`![](http://www.bieg.nl/beeld/woningen.jpg)`))
+        .then(() => bot.say(`%[Starters Hypotheek](postback:hypotheektype_starter) %[Nieuwe hypotheek](postback:hypotheektype_nieuw) %[Hypotheek oversluiten](postback:hypotheektype_oversluiten)`))
         .then(() => 'selecteerHypotheek');
     }
 },
