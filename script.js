b4078ca74a26de5cadd64e8ece388a5effd84301
bieg <mailme@bieg.nl>
2016-10-04 22:40:59 +0200

    sf
    fs

--- script.js
+++ script.js
@@ -25,17 +25,17 @@ if ( myDay.getHours() >=8 && myDay.gethours()<=12 )
   groet = "Goeiemorgen 🌝 ";
 }
 else  /* Hour is from noon to 5pm (actually to 5:59 pm) */
-if ( myDate.getHours() >= 12 && myDate.getHours() <= 17 )
+if ( myDay.getHours() >= 12 && myDay.getHours() <= 17 )
 {
 groet = "Goedendag 🌞";
 }
 else  /* the hour is after 5pm, so it is between 6pm and midnight */
-if ( myDate.getHours() > 17&& myDate.getHours() <= 20 )
+if ( myDay.getHours() > 17&& myDay.getHours() <= 20 )
 {
 groet = "Goedenavond 🌙";
 }
 else
-if (myDate.getHours() > 20  || myDate.getHours() <= 24 )
+if (myDay.getHours() > 20  || myDay.getHours() <= 24 )
 {
   groet = "Goedenavond 🌙  Bedankt voor je bezoek maar op dit moment is Independer echter gesloten. 🕘 Uiteraard kun je met onze IndyBot verder praten maar er is helaas niemand die jouw vraag specifiek kan beantwoorden. Je kan je vraag ook doormailen 📩 naar info@independer. Dan komt het altijd goed.";
 }
