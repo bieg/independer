da6af13e72bb2e74b9b128a711bbad3bd10ddc9c
bieg <mailme@bieg.nl>
2016-10-04 09:00:35 +0200

    script
    ip

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ start: {
                             .then(() => 'vervolgVragen')
                             break;
                         default:
-                          receive => 'processing'
+                          receive => 'speak'
                           break;
                       }
                     }
