14f266c5288b231ba6948defb1d33090f787be26
bieg <mailme@bieg.nl>
2016-10-05 23:06:37 +0200

    s
    sisc

--- script.js
+++ script.js
@@ -177,9 +177,10 @@ update_ja: {
           return bot.setProp('emailer', emailer)
               .then(()  => bot.say(`Ok - ✉️  dan hou ik je via ${emailer} op de hoogte.`))
               .then(   function() {
+
                                     Smooch.updateUser({
-                                            givenName: 'Doctor',
-                                            surname: 'Who'
+                                            givenName: 'Sjaak',
+                                            surname: 'Raak'
                                     })
                             })
             .then(()  =>'lastCheck')
