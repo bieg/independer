0f49b55dfe7df52e1370d0314c491fc618c7bf32
bieg <mailme@bieg.nl>
2016-10-05 21:03:10 +0200

    dw
    dw

--- script.js
+++ script.js
@@ -176,7 +176,7 @@ update_ja: {
           const emailer=message.text;
           return bot.setProp('emailer', emailer)
               .then(()  => bot.say(`Ok - ✉️  dan hou ik je via ${emailer} op de hoogte.`))
-              .then(  () =>  function() {
+              .then(  function() {
                                     Smooch.updateUser({
                                             givenName: 'Doctor',
                                             surname: 'Who'
