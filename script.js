fdc403a2b51654077671ece408b3ea2455cf1036
bieg <mailme@bieg.nl>
2016-10-11 20:49:19 +0200

    d
    wf

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 'use strict';
 
+const _ = require('lodash');
 const smoochBot = require('smooch-bot');
 const MemoryStore = smoochBot.MemoryStore;
 const MemoryLock = smoochBot.MemoryLock;
 const Bot = smoochBot.Bot;
-const Script = smoochBot.Script;
-const StateMachine = smoochBot.StateMachine;
-
-const _ = require('lodash');
 const Script = require('smooch-bot').Script;
+const StateMachine = smoochBot.StateMachine;
 const scriptRules = require('./script.json');
 
 class ConsoleBot extends Bot {
@@ -26,21 +24,21 @@ class ConsoleBot extends Bot {
 
 module.exports = new Script({
 
- processing: {
-prompt: (bot) => bot.say('![](http://www.bieg.nl/beeld/speechbubble.gif)'),
- receive: () => 'processing'
+processing: {
+      prompt: (bot) => bot.say('![](http://www.bieg.nl/beeld/speechbubble.gif)'),
+       receive: () => 'processing'
  },
 
 start: {
-    receive: (bot,message) => {
-        return bot.say(`${groet}... Wat voor soort hypotheek zoek je? `)
-        .then(() => bot.say(`![](http://www.bieg.nl/beeld/woningen.jpg)`))
-        .then(() => bot.say(`%[Starters Hypotheek](postback:hypotheektype_starter)`))
-        .then(() => bot.say (`%[Nieuwe hypotheek](postback:hypotheektype_nieuw) `))
-        .then(() => bot.say (`%[Hypotheek oversluiten](postback:hypotheektype_oversluiten)`))
-        .then(() => 'selecteerHypotheek');
+      receive: (bot,message) => {
+          return bot.say(`${groet}... Wat voor soort hypotheek zoek je? `)
+          .then(() => bot.say(`![](http://www.bieg.nl/beeld/woningen.jpg)`))
+          .then(() => bot.say(`%[Starters Hypotheek](postback:hypotheektype_starter)`))
+          .then(() => bot.say (`%[Nieuwe hypotheek](postback:hypotheektype_nieuw) `))
+          .then(() => bot.say (`%[Hypotheek oversluiten](postback:hypotheektype_oversluiten)`))
+          .then(() => 'selecteerHypotheek');
     }
-}
+    }
 });
 
 const userId = 'testUserId';
